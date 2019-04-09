fe930ca49624b4c9957df20ac97e26f2ec0e098f
张波 <470577391@qq.com>
2019-04-09 17:21:38 +0800

    Initial commit

---
+++ hotkey/hotkey.lua
@@ -0,0 +1,26 @@
+local launch_or_focus_window = function(name)
+	return function()
+		local app = hs.appfinder.appFromName(name)
+	    if app:isHidden() then
+	        hs.application.launchOrFocusByBundleID(app:bundleID())
+	        return
+	    else
+	    	app:hide()
+	    end
+	end
+end
+
+hs.hotkey.bind({"cmd"}, "L", hs.caffeinate.systemSleep)
+
+hs.fnutils.each({
+    {key = "C", app = "Google Chrome"},
+    {key = "E", app = "YoudaoDict"},
+    {key = "F", app = "Finder"},
+    {key = "I", app = "IntelliJ IDEA"},
+    {key = "L", app = "Sublime Text"},
+    {key = "Q", app = "WeChat Work"},
+    {key = "T", app = "iTerm2"},
+    {key = "W", app = "WeChat"}
+}, function(meta)
+    hs.hotkey.bind("alt", meta.key, launch_or_focus_window(meta.app))
+end)
---
+++ init.lua
@@ -0,0 +1,14 @@
+print('==================================================')
+
+-- require "autoscript.autoscript"
+require "clipboard.clipboard"
+require "hotkey.hotkey"
+require "ime.ime"
+require "reload.reload"
+-- require "volume.volume"
+require "window.window"
+
+
+-- if (hs.host.localizedName() == 'zhangbo的MacBook Pro') then
+-- 	require("autoscript.autoscript")
+-- end
